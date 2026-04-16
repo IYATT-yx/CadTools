a9157294cb38e40f102c62d97ea5ef670725a2df
IYATT-yx <iyatt@iyatt.com>
2026-04-16 23:29:59 +0800

    增加执行命令实现

--- Commands.cpp
+++ Commands.cpp
@@ -21,4 +21,32 @@ namespace Commands
 			acedRegCmds->addCommand(cmdGroup, shortCommandName.constPtr(), shortCommandName.constPtr(), commandInfo.flags, commandInfo.proc);
 		}
 	}
+
+	void executeCommand(const Commands::CommandList& cmdList, AcApDocument* pDoc)
+	{
+        if (cmdList.empty())
+        {
+            return;
+        }
+
+        if (pDoc == nullptr)
+        {
+            pDoc = acDocManager->curDocument();
+        }
+
+        if (pDoc == nullptr)
+        {
+            return;
+        }
+
+        AcString finalCmd;
+        finalCmd.format(L"._%s\n", cmdList[0]);
+        for (size_t i = 1; i < cmdList.size(); ++i)
+        {
+            finalCmd.append(cmdList[i]);
+            finalCmd.append(L"\n");
+
+        }
+        acDocManager->sendStringToExecute(pDoc, finalCmd.constPtr(), false, true, true);
+	}
 }
